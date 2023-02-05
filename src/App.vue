1bde69bbc612cf2face7e770f26685bed665dba7
Sveta <svetaantonyan00@gmail.com>
2023-02-06 00:35:35 +0400

    sortable components

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,16 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <dragg />
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import Dragg from "@/components/Dragg";
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    Dragg
   }
 }
 </script>
---
+++ src/components/Dragg.vue
@@ -0,0 +1,35 @@
+<template>
+  <draggable v-model="items">
+    <template v-slot:item="{item}">
+      <!-- example -->
+      <div>
+        {{item.title}}
+      </div>
+      <!-- or your own template -->
+    </template>
+  </draggable>
+</template>
+
+<script>
+import Draggable from "vue3-draggable";
+
+export default {
+  name: "Dragg",
+  components: {
+    Draggable,
+  },
+  data(){
+    return{
+      items:[
+        {title:'Aaa'},
+        {title:'Bbb'},
+        {title:'Ccc'}
+      ]
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
