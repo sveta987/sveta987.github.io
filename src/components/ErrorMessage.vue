fde14570402626c56f14ca2208876e8daafe9a0a
sveta987 <51134874+sveta987@users.noreply.github.com>
2023-02-09 15:28:52 +0400

    button colors changes

--- src/components/ErrorMessage.vue
+++ src/components/ErrorMessage.vue
@@ -3,7 +3,7 @@
 <div class="flex justify-center">
   <img src="../assets/images/error.png" alt="error-icon" class="w-[50px] h-[50px]">
 </div>
-  <h5 class="text-center text-3xl my-[15px]">Oh snap!</h5>
+  <h5 class="text-center text-3xl my-[15px]"> Oh snap! </h5>
   <slot></slot>
   <div class="flex justify-center">
     <button class="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-lg text-sm px-10 py-1.5 mr-2 mt-2"
